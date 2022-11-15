a9e09b7a58f352ca6da0cbfd360f938289b018a4
ryannicholas <ryannicholas.vieira@gmail.com>
2022-11-15 12:04:22 -0300

    refactor: :fire: remove unused functions and packages

--- back-end/src/services/transactionService.ts
+++ back-end/src/services/transactionService.ts
@@ -4,7 +4,7 @@ import { checkError } from '../middlewares/errorHandler';
 import authRepository from '../repositories/authRepository';
 import transactionsRepository from '../repositories/transactionRepository';
 import accountsRepository from '../repositories/accountRepository';
-import { formatDate, tomorrow } from '../utils/transactionUtils';
+import { tomorrow } from '../utils/transactionUtils';
 
 async function cashOut(user:IUserInfo, receiver:ReceiverInfo) {
     if(user.username === receiver.username) throw checkError(409, "Can`t cashout to yourself!");
--- back-end/src/utils/transactionUtils.ts
+++ back-end/src/utils/transactionUtils.ts
@@ -1,7 +1,3 @@
-export function formatDate(date:Date) {  
-    return date.toISOString().split('T')[0];
-};
-
 export function tomorrow(date:any) {
     const tomorrow = new Date(date);
     return tomorrow.setDate(tomorrow.getDate() + 1); 
